c0f6b690d546cfb20e173b0d8fe530d48eb6a950
codeindevelopment <codeindevelopment@gmail.com>
2022-01-23 11:01:09 +0330

    implement some configs and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "@route/Router";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+
+    
+
+        {/* Main Router File */}
+        <Router/>
+
+      </BrowserRouter>
+    </>
+  );
+}
